c59f536c200fbf6cca2f94a26fe553450a5ab5d5
Chanin Nusang <6110110589@psu.ac.th>
2021-09-07 15:27:05 +0700

    folders organize

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-import 'package:movie_application/api/api_client.dart';
+import 'package:movie_application/data/core/api_client.dart';
 import 'package:movie_application/app.dart';
-import 'package:movie_application/data/movie_remote_data_source_impl.dart.dart';
+import 'package:movie_application/data/data_sources/movie_remote_data_source_impl.dart';
 
 void main() {
   MovieRemoteDataSource dataSource =
